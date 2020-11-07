1dc65a1c703bfab1de09806c724a81f03ce4f779
= <=>
2020-11-07 04:49:38 -0800

    adding static viewer

---
+++ lite/app.js
@@ -0,0 +1,111 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "environment" }, audio: false };
+var track = null;
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+   // cameraOutput.src = cameraSensor.toDataURL("image/webp");
+   // cameraOutput.classList.add("taken");
+   // let element = $("#imageDIV"); // global variable
+    //var getCanvas; // global variable
+    document.body.style.backgroundImage = cameraSensor.toDataURL("image/webp");
+    html2canvas(document.getElementById("camera"),{ removeContainer: false, backgroundColor: null}).then(canvas => {
+        //document.body.appendChild(canvas.id="canvas-out");
+/*        let imgageData = canvas.toDataURL("image/png");
+        var newData = imageData.replace(/^data:image\/png/, "data:application/octet-stream");*/
+        //window.location = mewData;
+       window.location = canvas.toDataURL("image/png").replace(/^data:image\/png/, "data:application/octet-stream");
+    });
+    //console.log(getCanvas);
+  // Now browser starts downloading it instead of just showing it
+  // track.stop();
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
+
+//drag an resizable
+interact('#mydiv')
+  .resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: true, top: true },
+
+    listeners: {
+      move (event) {
+        var target = event.target
+        var x = (parseFloat(target.getAttribute('data-x')) || 0)
+        var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+        // update the element's style
+        target.style.width = event.rect.width + 'px'
+        target.style.height = event.rect.height + 'px'
+
+        // translate when resizing from top or left edges
+        console.log("#### get original ####");
+        console.log(target);
+        console.log(target.style);
+        console.log(target.style.transform);
+        console.log(event);
+        console.log(event.deltaRect);
+        console.log(event.deltaRect.left);
+        console.log(event.deltaRect.top);
+        x += event.deltaRect.left
+        y += event.deltaRect.top
+
+	console.log(x,y,target.style.transform);
+        target.style.webkitTransform = target.style.transform =
+          'translate(' + x + 'px,' + y + 'px)'
+
+        target.setAttribute('data-x', x)
+        target.setAttribute('data-y', y)
+        // target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+      }
+    },
+    modifiers: [
+      // keep the edges inside the parent
+      interact.modifiers.restrictEdges({
+        outer: 'parent'
+      }),
+
+      // minimum size
+      interact.modifiers.restrictSize({
+        min: { width: 100, height: 50 }
+      })
+    ],
+
+    inertia: true
+  })
+  .draggable({
+    listeners: { move: window.dragMoveListener },
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ]
+  })
